fa7478fc1ce2346acb720abf57c8080e8c0231ff
Hector.D.Rguez@gmail.com <Hector.D.Rguez@gmail.com>
2020-06-28 21:28:05 +0100

    Subimos un ejemplo del config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 """ Configuración de variables del sistema
 """
 # Referencia interna de nuestra instalación, necesaria para conocer el estado de la batería y del consumo
-VictronInterna = 'c8df84d39633'
+VictronInterna = 'xxxxxxxxx'
 # Dirección IP del Venux/ColorControl GX en caso de que el programa se use en otro equipo. En caso contrario dejar el actual
 Venus = 'localhost'
 # Dirección de Email a donde mandar los correos de estado
